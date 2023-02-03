631fac84311241c22ee6f0f6ce519a262373c654
WPC Online Conference <wpnconlineconference@protonmail.com>
2023-02-03 12:14:25 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
+  content: [
+    "./pages/**/*.html",
+  ],
+}
